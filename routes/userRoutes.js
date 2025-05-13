146be8877eb76a7aac1477ba230dabff7f728164
Annas Putra Anuraga <annasputra52@gmail.com>
2025-05-13 20:19:19 +0700

    asd

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,7 @@ router.post('/login', userController.loginUser);
 
 // Example protected route (requires JWT)
 router.get('/profile', userController.protect, (req, res) => {
-    res.json({ message: 'Profile data', user: req.user });
+  res.status(200).json({ message: 'Profile data', user: req.user }); // Tambahkan status 200
 });
 
 // Rute register
