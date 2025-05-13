9ff07ec487fb77819abc2cfc266e5bc37d3a876b
Annas Putra Anuraga <annasputra52@gmail.com>
2025-05-13 19:37:20 +0700

    verceldeploy#1

--- server.js
+++ server.js
@@ -20,8 +20,20 @@ connectDB();
 // Routes
 app.use('/api/users', userRoutes);
 
+// Tangani rute root (/)
 app.get('/', (req, res) => {
-    res.send('API is running!');
+    res.send('<h1>API is running!</h1><p>Coba akses /api/users untuk melihat data pengguna.</p>');
+});
+
+// Middleware untuk menangani rute yang tidak ditemukan (404)
+app.use((req, res, next) => {
+    res.status(404).send('<h1>404 Not Found</h1><p>Rute tidak ditemukan.</p>');
+});
+
+// Middleware untuk menangani kesalahan server (500)
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('<h1>500 Internal Server Error</h1><p>Terjadi kesalahan pada server.</p>');
 });
 
 // Start the server
